bbadf780d5e83653047e7f15549767df825c6577
VinodCGI <kvinodk4444@gmail.com>
2017-05-04 22:07:15 +0530

    Add files via upload

---
+++ 5.2app.js
@@ -0,0 +1,9 @@
+//Object which holds the person details
+var personObj={name:"Vinod",age:"25",dob:"12/12/1992",pob:"Bangalore"};
+
+console.log(personObj.name); // To display Name
+console.log(personObj.age);  // To display age
+console.log(personObj.dob);  // To display Date of Birth
+console.log(personObj.pob);  // To display Place Of Birth
+
+//QUERY : Is there any way that I can use /n like that in order to display in new line.? 
